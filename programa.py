dcd140b33078776185ee04edf9842fc3ee397b5f
Mecoboy <ca8619933@gmail.com>
2025-09-28 20:54:10 -0600

    Tercer commit: V2

--- programa.py
+++ programa.py
@@ -2,6 +2,7 @@
 # Solicitar el nombre y genero de la persona 
 nombre = input("Ingresa tu nombre: ").strip().title()
 sexo = input("Ingresa tu Genero (M para mujer, H para hombre): ").strip().upper()
+edad_str = input("Ingresa tu edad: ").strip()
 
 # Obtener la primera letra del nombre
 pra_letra = nombre[0].upper()
@@ -14,7 +15,19 @@ else:
     # Si NO cumple ninguna de las condiciones, asignar al grupo B
     grupo = "B"
 
+    # Validar la edad y determinar si es mayor de edad (18 años o más)
+try:
+    edad = int(edad_str)
+    if edad >= 18:
+        estado_edad = "Eres mayor de edad."
+    else:
+        estado_edad = "Eres menor de edad."
+except ValueError:
+    # Manejo de error si el usuario no introduce un número
+    estado_edad = "No fue posible determinar tu mayoría de edad."
+
 # Mostrar los resultados al usuario
 print(f"\nHola {nombre}")
 print(f"Tu genero es: {'Mujer' if sexo == 'M' else 'Hombre'}") # "Mujer" si sexo es "M", "Hombre" en cualquier otro caso
 print(f"Perteneces al grupo: {grupo}")
+print(estado_edad)
